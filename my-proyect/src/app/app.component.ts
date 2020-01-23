b861e9933f9664272c390d31bdfe687e70ad457e
Alexander <alexchilaac@gmail.com>
2020-01-23 13:55:52 -0500

    creacion de componente hello, avance de directivas y eventos

--- my-proyect/src/app/app.component.ts
+++ my-proyect/src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-proyect';
+
+  //Esta es una variable que fue utilizada para el app.component.html
+  //para asignar este valor a otra variable :: name :: declarada en el hello.component.ts
+  // se usó para ejecutar Property bindings.
+  public myName: string = "Alexander"
+
+  //se declaran estas variables para asignarle un estilo al titulo del proyecto dando clic en el nombre 
+  public tittleStyles:{};
+  public counter = 0; 
+
+  //onSayHello se ejecuta cuando se ejecuta el event binding sayHello que se encuen tra en app.component.html
+  onSayHello(message){
+    console.log(message);
+    //cada vez que entra al metodo se ejecuta eta funcion y se suma uno al contador   
+    this.counter ++ ; 
+    this.updateTittleStyles()
+  }
+
+  //se ejecuta cuando la clase [ngClass] se ejecuta en app.component.html
+  updateNameClasses(name:string){
+    return {
+    'error' : name.length <=3,
+    'warning': name.length > 3 && name.length <=6,
+    'success': name.length >6 ,
+    'bold' : name.length >8
+  }
+  }
+  // llama el objeto tittleStyles y le asigna un margen y le cambia el color validando el contador 
+  updateTittleStyles(){
+    this.tittleStyles = {
+      'margin-top' : '40px',
+      'color' : this.counter < 3 ? 'green' : 'goldenrod'
+    }
+  }
 }
